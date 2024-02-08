0f70b895f238d4ffce08b677be4c678b08684b66
tsuyoshi.kumamoto <tsuyoshi.kumamoto@play.jp>
2024-02-08 10:57:09 +0900

    recoilチュートリアルをreacinativeで実装

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Recoil from 'recoil';
+import { TextInput, View, Text } from 'react-native';
 
-export default function App() {
+// atom
+const textState = Recoil.atom({
+  key: 'textState', // unique ID (with respect to other atoms/selectors)
+  default: 'aaaaa', // default value (aka initial value)
+});
+
+// selector
+const charCountState = Recoil.selector({
+  key: 'charCountState', // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+// Components
+function CharacterCount() {
+  const count = Recoil.useRecoilValue(charCountState);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>
+        Character Count: {count}
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function _TextInput() {
+  const [text, setText] = Recoil.useRecoilState(textState);
+
+  const onChangeText = (text) => {
+    setText(text);
+  };
+
+  return (
+    <View>
+      <TextInput style={{ width: 100, height: 30, borderColor: 'black', borderWidth: 1 }} value={text} onChangeText={onChangeText} />
+      <Text>Echo: {text}</Text>
+    </View>
+  );
+}
+
+function CharacterCounter() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <_TextInput />
+      <CharacterCount />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <Recoil.RecoilRoot>
+      <CharacterCounter />
+    </Recoil.RecoilRoot>
+  );
+}
